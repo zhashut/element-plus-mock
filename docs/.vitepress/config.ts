6e38160fc1118f3b9b49a1a89a5c827f19c156f7
zhashut <2105798885@qq.com>
2024-12-25 23:59:16 +0800

    Updates

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,57 @@
+import { defineConfig } from 'vitepress'
+import { fileURLToPath, URL } from 'node:url'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueMacros from 'unplugin-vue-macros'
+import { containerPreview, componentPreview } from '@vitepress-demo-preview/plugin'
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Element-Plus-Mock",
+  vite: {
+    plugins: [
+      VueMacros.vite({
+        setupComponent: false,
+        setupSFC: false,
+        plugins: {
+          vueJsx: vueJsx(),
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('../../src', import.meta.url))
+      }
+    }
+  },
+  markdown: {
+    config(md) {
+      md.use(containerPreview)
+      md.use(componentPreview)
+    }
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [],
+
+    sidebar: [
+      {
+        text: '组件',
+        items: [
+          { text: 'Button', link: '/components/button' },
+          { text: 'Collapse', link: '/components/collapse' },
+          { text: 'Icon', link: '/components/icon' },
+          { text: 'Tooltip', link: '/components/tooltip' },
+          { text: 'Dropdown', link: '/components/dropdown' },
+          { text: 'Message', link: '/components/message' },
+          { text: 'Input', link: '/components/input' },
+          { text: 'Switch', link: '/components/switch' },
+          { text: 'Select', link: '/components/select' },
+          { text: 'Form', link: '/components/form' },
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
